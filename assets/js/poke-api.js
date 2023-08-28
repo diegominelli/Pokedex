af01768ec667a8b0a645d93e945a5474f7ab5a38
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-08-28 16:32:26 -0300

    manipulando as multiplas requisicoes em paralelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,15 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
-  .then((response) => response.json())
-  .then((jsonBoby) => jsonBoby.results)
-  .catch((error) => console.error(error))
+    .then((response) => response.json())
+    .then((jsonBoby) => jsonBoby.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailsRequets) => Promise.all(detailsRequets))
+    .then((pokemonsDetails) => pokemonsDetails)   
 }
